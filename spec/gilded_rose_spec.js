23386dad13e897e4f341e8ce5267c0c39f7f5af7
Paul Smart <pdsmart6@gmail.com>
2022-02-02 16:02:17 +0000

    added suite of unit tests

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,12 +1,6 @@
 const {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
-  });
-
   it("a normal item decreases in quality", function() {
     const gildedRose = new Shop([ new Item("random", 1, 40) ]); 
   const items = gildedRose.updateQuality();
@@ -14,7 +8,18 @@ describe("Gilded Rose", function() {
   expect(items[0].sellIn).toEqual(0)
   })
 
-  it("a normal item decreases in quality", function() {
+  it("an in-date normal item decreases in quality", function() {
+    const gildedRose = new Shop([ new Item("random", 5, 40) ]);
+    for (let i = 0; i < 4; i++) {
+      gildedRose.updateQuality()
+      
+    }
+  const items = gildedRose.updateQuality();
+  expect(items[0].quality).toEqual(35)
+  expect(items[0].sellIn).toEqual(0)
+  })
+
+  it("an expired normal item decreases in quality two times as fast", function() {
     const gildedRose = new Shop([ new Item("random", 1, 40) ]); 
   gildedRose.updateQuality();
   gildedRose.updateQuality();
@@ -35,7 +40,38 @@ describe("Gilded Rose", function() {
   expect(items[0].quality).toEqual(21)
   })
 
-  it("run main test", () => {
+  it("expired aged Brie increases in quality twice as fast", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 1, 20) ])
+    gildedRose.updateQuality()
+  const items = gildedRose.updateQuality()
+  expect(items[0].quality).toEqual(23)
+  })
+
+  it("backstage passes increase in value before the concert", function() {
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 11, 5) ])
+  const items = gildedRose.updateQuality()
+  expect(items[0].quality).toEqual(6)
+  })
+
+  it("backstage passes increase further value 10 days before the concert", function() {
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 10, 5) ])
+  const items = gildedRose.updateQuality()
+  expect(items[0].quality).toEqual(7)
+  })
+
+  it("backstage passes increase even further value 5 days before the concert", function() {
+    const gildedRose = new Shop([ new Item('Backstage passes to a TAFKAL80ETC concert', 5, 5) ])
+  const items = gildedRose.updateQuality()
+  expect(items[0].quality).toEqual(8)
+  })
+
+  it("backstage passes are worthless after the concert", function() {
+    const gildedRose = new Shop([ new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50) ])
+  const items = gildedRose.updateQuality()
+  expect(items[0].quality).toEqual(0)
+  })
+
+  it("deals with a large input", () => {
     const inventory = [
       new Item("+5 Dexterity Vest", 10, 20),
       new Item("Aged Brie", 2, 0),
