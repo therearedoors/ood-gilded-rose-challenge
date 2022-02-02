f09ea9d2e7ac96a22d4de63b26c1b3a1e22c4c1f
Paul Smart <pdsmart6@gmail.com>
2022-02-02 14:58:39 +0000

    Added some tests

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,10 +1,68 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
+const {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
   it("should foo", function() {
     const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("fixme");
+    expect(items[0].name).toEqual("foo");
   });
+
+  it("a normal item decreases in quality", function() {
+    const gildedRose = new Shop([ new Item("random", 1, 40) ]); 
+  const items = gildedRose.updateQuality();
+  expect(items[0].quality).toEqual(39)
+  expect(items[0].sellIn).toEqual(0)
+  })
+
+  it("a normal item decreases in quality", function() {
+    const gildedRose = new Shop([ new Item("random", 1, 40) ]); 
+  gildedRose.updateQuality();
+  gildedRose.updateQuality();
+  gildedRose.updateQuality();
+  expect(gildedRose.items[0].quality).toEqual(35)
+  expect(gildedRose.items[0].sellIn).toEqual(-2)
+  })
+
+  it("sulfuras can't decrease in quality", function() {
+    const gildedRose = new Shop([ new Item("Sulfuras, Hand of Ragnaros", 1, 80) ]);
+  const items = gildedRose.updateQuality();
+  expect(items[0].quality).toEqual(80)
+  })
+
+  it("aged Brie increases in quality", function() {
+    const gildedRose = new Shop([ new Item("Aged Brie", 1, 20) ]); 
+  const items = gildedRose.updateQuality();
+  expect(items[0].quality).toEqual(21)
+  })
+
+  it("run main test", () => {
+    const inventory = [
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      new Item("Conjured Mana Cake", 3, 6)
+    ]
+    const expected = [
+    new Item("+5 Dexterity Vest", -20, 0),
+    new Item("Aged Brie", -28, 50),
+    new Item("Elixir of the Mongoose", -25, 0),
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", -15, 0),
+    new Item("Backstage passes to a TAFKAL80ETC concert", -20, 0),
+    new Item("Backstage passes to a TAFKAL80ETC concert", -25, 0),
+    new Item("Conjured Mana Cake", -27, 0)
+    ]
+    const gildedRose = new Shop(inventory)
+    for (let i = 0; i < 29; i++) {
+      gildedRose.updateQuality()
+    }
+    expect(gildedRose.updateQuality()).toEqual(expected)
+  })
 
 });
